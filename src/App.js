20b9e47e203cec338b795ad933f17f7212ed131a
Ali Abbani <aliabbani08@gmail.com>
2023-01-01 10:03:29 +0100

    start the app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Palais
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+// import Slider from "../components/Slider.js";
+// import Announcement from "../components/Announcement.js";
+// import Navbar from "../components/Navbar.js";
+// import Categories from "../components/Categories.js";
+// import Products from "../components/Products.js";
+// import Newsletter from "../components/Newsletter.js";
+// import Footer from "../components/Footer.js";
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <Announcement />
+//       <Navbar />
+//       <Slider />
+//       <Categories />
+//       <Products />
+//       <Newsletter />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Home;
