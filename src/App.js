245af27149ab983278d135e288a230d7fed83e8d
Ali Abbani <aliabbani08@gmail.com>
2023-01-03 15:04:00 +0100

    Add a navbar to start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      Palais
+    <div className="App border border-black">
+      <NavBar />
+      <div className="text-3xl font-bold underline text-blue-500">Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
+      <div>Palais</div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const NavBar = () => {
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  return (
+    <div>
+      <nav className="flex flex-row w-full justify-between bg-black">
+        <div className="flex flex-row justify-between items-center text-white uppercase p-6 w-full md:w-max">
+          <div>Palais</div>
+          <AiOutlineMenu
+            className="flex md:hidden text-2xl text-yellow-600"
+            onClick={() => setShowMenu(true)}
+          />
+        </div>
+        {showMenu ? (
+          // mobile
+          <div className="bg-white w-full h-full absolute top-0 left-0 overflow-scroll">
+            <div className="text-black bg-silver p-6 bg-stone-200 flex flex-row justify-between items-center ">
+              <div className="uppercase">Palais</div>
+              <AiOutlineClose
+                className="text-2xl"
+                onClick={() => setShowMenu(false)}
+              />
+            </div>
+            <div className="items-center justify-evenly opacity-95 text-white uppercase h-full flex flex-col text-black">
+              <div>brand</div>
+              <div>restaurants</div>
+              <div>loyality card</div>
+              <div>careers</div>
+              <div>contact</div>
+              <a href="https://menu.omegasoftware.ca/lepalais1">menu</a>
+            </div>
+          </div>
+        ) : (
+          // desktop
+          <div className="hidden md:flex flex-row justify-between items-center bg-black py-7 px-4">
+            <div className="flex md:gap-5 lg:gap-10 text-white uppercase">
+              <div>brand</div>
+              <div>restaurants</div>
+              <div>loyality card</div>
+              <div>careers</div>
+              <div>contact</div>
+              <a href="https://menu.omegasoftware.ca/lepalais1">Menu</a>
+              <div className="text-yellow-700">+243 999</div>
+            </div>
+          </div>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
