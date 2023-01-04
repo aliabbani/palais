5117fae1639f54f0c2964a5cc07d6c7204e9ef7f
Ali Abbani <aliabbani08@gmail.com>
2023-01-04 21:52:51 +0100

    up

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#272a31] text-white">
+      <div>
+        © 2022 Le Palais Restaurants | ALL RIGHTS RESERVED | DEVELOPED BY Ali
+      </div>
+      <div>fb</div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Brand.js
+++ src/pages/Brand.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import "./Brand.css";
 
@@ -8,78 +9,80 @@ const Brand = () => {
   return (
     <div>
       <NavBar />
-
-      <div className="brand-main">
-        <div className="flex flex-col md:flex-row justify-between pb-10">
-          <div className="brand uppercase text-2xl md:text-4xl">brand</div>
-          <div className="flex md:justify-between font-semibold pt-4">
-            <div
-              className="pr-6 cursor-pointer uppercase"
-              onClick={() => setStory(!story)}
-            >
-              <div>our story</div>
-            </div>
-            <div
-              className="cursor-pointer uppercase"
-              onClick={() => setStory(!story)}
-            >
-              our history
+      <div className="back">
+        <div className="brand-main pb-[180px] bg-slate-300">
+          <div className="flex flex-col md:flex-row justify-between pb-10">
+            <div className="brand uppercase text-2xl md:text-4xl">brand</div>
+            <div className="flex md:justify-between font-semibold pt-4">
+              <div
+                className="pr-6 cursor-pointer uppercase"
+                onClick={() => setStory(!story)}
+              >
+                <div>our story</div>
+              </div>
+              <div
+                className="cursor-pointer uppercase"
+                onClick={() => setStory(!story)}
+              >
+                our history
+              </div>
             </div>
           </div>
+          {story ? (
+            <div>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas dictum posuere nulla, elementum ornare tellus dapibus
+                eu. Nulla facilisi. Mauris at congue lacus, quis rutrum nulla.
+              </p>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas dictum posuere nulla, elementum ornare tellus dapibus
+                eu. Nulla facilisi. Mauris at congue lacus, quis rutrum nulla.
+              </p>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas dictum posuere nulla, elementum ornare tellus dapibus
+                eu. Nulla facilisi. Mauris at congue lacus, quis rutrum nulla.
+              </p>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas dictum posuere nulla, elementum ornare tellus dapibus
+                eu. Nulla facilisi. Mauris at congue lacus, quis rutrum nulla.
+              </p>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          ) : (
+            <div>
+              <p className="px-28 pb-5">
+                1985 Ali, elementum ornare tellus dapibus eu. Nulla facilisi.
+                Mauris at congue lacus, quis rutrum nulla.
+              </p>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas dictum posuere nulla, elementum ornare tellus dapibus
+                eu. Nulla facilisi. Mauris at congue lacus, quis rutrum nulla.
+              </p>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas dictum posuere nulla, elementum ornare tellus dapibus
+                eu. Nulla facilisi. Mauris at congue lacus, quis rutrum nulla.
+              </p>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas dictum posuere nulla, elementum ornare tellus dapibus
+                eu. Nulla facilisi. Mauris at congue lacus, quis rutrum nulla.
+              </p>
+              <p className="px-28 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          )}
         </div>
-        {story ? (
-          <div>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              dictum posuere nulla, elementum ornare tellus dapibus eu. Nulla
-              facilisi. Mauris at congue lacus, quis rutrum nulla.
-            </p>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              dictum posuere nulla, elementum ornare tellus dapibus eu. Nulla
-              facilisi. Mauris at congue lacus, quis rutrum nulla.
-            </p>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              dictum posuere nulla, elementum ornare tellus dapibus eu. Nulla
-              facilisi. Mauris at congue lacus, quis rutrum nulla.
-            </p>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              dictum posuere nulla, elementum ornare tellus dapibus eu. Nulla
-              facilisi. Mauris at congue lacus, quis rutrum nulla.
-            </p>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </div>
-        ) : (
-          <div>
-            <p className="px-28 pb-5">
-              1985 Ali, elementum ornare tellus dapibus eu. Nulla
-              facilisi. Mauris at congue lacus, quis rutrum nulla.
-            </p>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              dictum posuere nulla, elementum ornare tellus dapibus eu. Nulla
-              facilisi. Mauris at congue lacus, quis rutrum nulla.
-            </p>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              dictum posuere nulla, elementum ornare tellus dapibus eu. Nulla
-              facilisi. Mauris at congue lacus, quis rutrum nulla.
-            </p>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              dictum posuere nulla, elementum ornare tellus dapibus eu. Nulla
-              facilisi. Mauris at congue lacus, quis rutrum nulla.
-            </p>
-            <p className="px-28 pb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </div>
-        )}
       </div>
+      <Footer />
     </div>
   );
 };
