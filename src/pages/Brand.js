3698825ec04b14bc12f76f8891a34abb4b18321d
Ali Abbani <aliabbani08@gmail.com>
2023-01-04 14:25:46 +0100

    up

--- src/pages/Brand.js
+++ src/pages/Brand.js
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/NavBar";
 import "./Brand.css";
 
 const Brand = () => {
+  const [story, setStory] = useState(false);
+
   return (
     <div>
       <NavBar />
 
       <div className="brand-main">
-        <div className="flex justify-between pb-10">
+        <div className="flex flex-col md:flex-row justify-between pb-10">
           <div className="brand uppercase">brand</div>
-          <div className="flex justify-between">
-            <div className="pr-6">our history</div>
-            <div>our story</div>
+          <div className="flex md:justify-between">
+            <div
+              className="pr-6 cursor-pointer "
+              onClick={() => setStory(!story)}
+            >
+              our story
+            </div>
+            <div className="cursor-pointer" onClick={() => setStory(!story)}>
+              our history
+            </div>
           </div>
         </div>
-        <p className="text-center px-60">
-          brand
-          pageeeeeeeeeeeeeeeeeeeeeeeeeeeeeee loren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren ipsum
-        </p>
+        {story ? (
+          <p className="text-center px-40">
+            brand pageeeeeeeeeeeeeeeeeeeeeeeeeeeeeee loren ipsum dolor sit amet
+            loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor
+            sit amet loren ipsumloren ipsum dolor sit amet loren ipsumloren
+            ipsum dolor sit amet loren ipsumloren ipsum dolor sit amet loren
+            ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit
+            amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum
+            dolor sit amet loren ipsumloren ipsum dolor sit amet loren
+            ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit
+            amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum
+            dolor sit amet loren ipsumloren ipsum dolor sit amet loren
+            ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit
+            amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum
+            dolor sit amet loren ipsumloren ipsum dolor sit amet loren
+            ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit
+            amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum
+            dolor sit amet loren ipsumloren ipsum dolor sit amet loren
+            ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit
+            amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum
+            dolor sit amet loren ipsumloren ipsum dolor sit amet loren
+            ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit
+            amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum
+            dolor sit amet loren ipsumloren ipsum dolor sit amet loren
+            ipsumloren ipsum dolor sit amet loren ipsumloren ipsum dolor sit
+            amet loren ipsumloren ipsum dolor sit amet loren ipsumloren ipsum
+            dolor sit amet loren ipsum
+          </p>
+        ) : (
+          <p className="text-center px-40">second</p>
+        )}
       </div>
     </div>
   );
