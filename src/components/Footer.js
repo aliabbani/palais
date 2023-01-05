148a73f1d28b1dd3c94ee5341357fc7a9023e89e
Ali Abbani <aliabbani08@gmail.com>
2023-01-05 18:43:16 +0100

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,13 @@ import { GrFacebookOption, GrInstagram } from "react-icons/gr";
 
 const Footer = () => {
   return (
-    <div className="bg-[#272a31] h-20 flex flex-col md:flex-row md:justify-between md:items-center text-[#d3d3d3] px-5 md:px-10">
-      <div className="text-sm md:text-base pb-2 md:pb-0">
-        © 2022 Le Palais Restaurants | All Rights Reserved | Developed By Ali
+    <div className="bg-[#272a31] h-24 md:pt-0 flex flex-col md:flex-row md:justify-between md:items-center text-[#d3d3d3] px-5 md:px-10">
+      <div className="text-sm md:text-base py-3 md:py-0">
+        © 2023 Le Palais Restaurants | All Rights Reserved | Developed By Ali
       </div>
       <div className="flex md:justify-between">
         <a
-          className="pr-6"
+          className="pr-4 md:pr-6"
           href="https://web.facebook.com/LePalaisRestaurantKinshasa/?_rdc=1&_rdr"
         >
           <GrFacebookOption />
