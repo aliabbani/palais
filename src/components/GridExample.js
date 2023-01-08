77ede2d4d9567e5452190f1667b9602a77d3cdd4
Ali Abbani <aliabbani08@gmail.com>
2023-01-08 13:22:07 +0100

    up

--- src/components/GridExample.js
+++ src/components/GridExample.js
@@ -10,71 +10,102 @@ function GridExample() {
   return (
     <div className="main px-2 md:px-2 py-10 md:py-10 justify-evenly flex flex-col md:flex-row md:flex-wrap gap-x-8 gap-y-8 md:gap-x-0 md:gap-y-8">
       {/* first */}
-      <div className="border border-slate-400 basis-5/12">
-        <div>
-          <img
-            className=""
-            src="https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-          />
-        </div>
-        <div className="px-2 py-4">
-          <h2 className="py-4 text-2xl font-extrabold">Palais</h2>
-          <div>
-            lorem icons lorem iconslorem icons lorem iconslorem iconslorem icons{" "}
+      <div class="flex justify-center">
+        <div class="rounded-lg shadow-lg bg-white max-w-lg">
+          <a href="#!">
+            <img
+              class="rounded-t-lg"
+              src="https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+              alt=""
+            />
+          </a>
+          <div class="p-6">
+            <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+            <p class="text-gray-700 text-base mb-4">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button
+              type="button"
+              class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Button
+            </button>
           </div>
         </div>
       </div>
       {/* second */}
-      <div className="border border-slate-400 basis-5/12">
-        <div>
-          <img
-            className=""
-            src="https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-          />
-        </div>
-        <div className="px-2 py-4">
-          <h2 className="py-4 text-2xl font-extrabold">Circle de Palais</h2>
-          <div>
-            lorem icons lorem iconslorem icons lorem iconslorem iconslorem
-            iconslorem icons lorem iconslorem icons lorem iconslorem iconslorem
-            iconslorem icons lorem iconslorem icons lorem iconslorem iconslorem
-            iconslorem icons lorem iconslorem icons lorem iconslorem iconslorem
-            iconslorem icons lorem iconslorem icons lorem iconslorem iconslorem
-            icons
+      <div class="flex justify-center">
+        <div class="rounded-lg shadow-lg bg-white max-w-lg">
+          <a href="#!">
+            <img
+              class="rounded-t-lg"
+              src="https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80"
+              alt=""
+            />
+          </a>
+          <div class="p-6">
+            <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+            <p class="text-gray-700 text-base mb-4">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button
+              type="button"
+              class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Button
+            </button>
           </div>
         </div>
       </div>
       {/* third */}
-      <div className="border border-slate-400 basis-5/12">
-        <div>
-          <img
-            className=""
-            src="https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-          />
-        </div>
-        <div className="px-2 py-4">
-          <h2 className="py-4 text-2xl font-extrabold">Le Petit Palais</h2>
-          <div>
-            lorem icons lorem iconslorem icons lorem iconslorem iconslorem icons{" "}
+      <div class="flex justify-center">
+        <div class="rounded-lg shadow-lg bg-white max-w-lg">
+          <a href="#!">
+            <img
+              class="rounded-t-lg"
+              src="https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+              alt=""
+            />
+          </a>
+          <div class="p-6">
+            <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+            <p class="text-gray-700 text-base mb-4">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button
+              type="button"
+              class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Button
+            </button>
           </div>
         </div>
       </div>
       {/* fourth */}
-      <div className="border border-slate-400 basis-5/12">
-        <div>
-          <img
-            className=""
-            src="https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-          />
-        </div>
-        <div className="px-2 py-4">
-          <h2 className="py-4 text-2xl font-extrabold">Royal Burger</h2>
-          <div>
-            lorem icons lorem iconslorem icons lorem iconslorlorem icons lorem
-            iconslorem icons lorem iconslorem iconslorem iconslorem icons lorem
-            iconslorem icons lorem iconslorem iconslorem iconslorem icons lorem
-            iconslorem icons lorem iconslorem iconslorem iconslorem icons lorem
-            iconslorem icons lorem iconslorem iconslorem icons
+      <div class="flex justify-center">
+        <div class="rounded-lg shadow-lg bg-white max-w-lg">
+          <a href="#!">
+            <img
+              class="rounded-t-lg"
+              src="https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+              alt=""
+            />
+          </a>
+          <div class="p-6">
+            <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+            <p class="text-gray-700 text-base mb-4">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button
+              type="button"
+              class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Button
+            </button>
           </div>
         </div>
       </div>
