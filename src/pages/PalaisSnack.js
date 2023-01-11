f7c904f53e8b327fe0583dc79d979aec67b52ed0
Ali Abbani <aliabbani08@gmail.com>
2023-01-12 00:22:12 +0100

    Add maps

--- src/pages/PalaisSnack.js
+++ src/pages/PalaisSnack.js
@@ -6,7 +6,120 @@ const PalaisSnack = () => {
   return (
     <div>
       <NavBar />
+      <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide relative"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner relative w-full overflow-hidden">
+          <div className="carousel-item active relative float-left w-full">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+              className="block w-full"
+              alt="Wild Landscape"
+            />
+          </div>
+          <div className="carousel-item relative float-left w-full">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+              className="block w-full"
+              alt="Camera"
+            />
+          </div>
+          <div className="carousel-item relative float-left w-full">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+              className="block w-full"
+              alt="Exotic Fruits"
+            />
+          </div>
+        </div>
+      </div>
       <div>Palais Snack</div>
+      {/* Maps & contact */}
+      <div className="container my-24 px-6 mx-auto">
+        <section className="mb-32 text-gray-800">
+          <div className="block rounded-lg shadow-lg bg-white">
+            <div className="flex flex-wrap items-center">
+              <div className="grow-0 shrink-0 basis-auto block w-full lg:flex lg:w-6/12 xl:w-4/12">
+                <div className="map-container-2 h-[500px] w-full">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d497.32452895858546!2d15.3113547!3d-4.2983301!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a330641c56e55%3A0x601ebf4d2e79c8f7!2sLe%20Palais%201%20Restaurant%20CTC!5e0!3m2!1sen!2scd!4v1673478785405!5m2!1sen!2scd"
+                    allowfullscreen
+                    className="left-0 top-0 h-full w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg"
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
+              </div>
+              <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
+                <div className="flex flex-wrap pt-12 lg:pt-0">
+                  <div className="mb-12 grow-0 shrink-0 basis-auto w-full md:w-6/12 lg:w-full xl:w-6/12 px-3 md:px-6 xl:px-12">
+                    <div className="flex items-start">
+                      <div className="shrink-0">
+                        <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                          <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fas"
+                            data-icon="headset"
+                            className="w-5 text-white"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M192 208c0-17.67-14.33-32-32-32h-16c-35.35 0-64 28.65-64 64v48c0 35.35 28.65 64 64 64h16c17.67 0 32-14.33 32-32V208zm176 144c35.35 0 64-28.65 64-64v-48c0-35.35-28.65-64-64-64h-16c-17.67 0-32 14.33-32 32v112c0 17.67 14.33 32 32 32h16zM256 0C113.18 0 4.58 118.83 0 256v16c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16v-16c0-114.69 93.31-208 208-208s208 93.31 208 208h-.12c.08 2.43.12 165.72.12 165.72 0 23.35-18.93 42.28-42.28 42.28H320c0-26.51-21.49-48-48-48h-32c-26.51 0-48 21.49-48 48s21.49 48 48 48h181.72c49.86 0 90.28-40.42 90.28-90.28V256C507.42 118.83 398.82 0 256 0z"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="grow ml-6">
+                        <p className="font-bold mb-1">
+                          Reservation & Enquiries
+                        </p>
+                        <p className="text-gray-500">
+                          lepalais1restaurant@gmail.com
+                        </p>
+                        <p className="text-gray-500">+243-907-626-431</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mb-12 grow-0 shrink-0 basis-auto w-full md:w-6/12 lg:w-full xl:w-6/12 px-3 md:px-6 xl:px-12">
+                    <div className="flex items-start">
+                      <div className="shrink-0">
+                        <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                          <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fas"
+                            data-icon="dollar-sign"
+                            className="w-3 text-white"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 288 512"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M209.2 233.4l-108-31.6C88.7 198.2 80 186.5 80 173.5c0-16.3 13.2-29.5 29.5-29.5h66.3c12.2 0 24.2 3.7 34.2 10.5 6.1 4.1 14.3 3.1 19.5-2l34.8-34c7.1-6.9 6.1-18.4-1.8-24.5C238 74.8 207.4 64.1 176 64V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48h-2.5C45.8 64-5.4 118.7.5 183.6c4.2 46.1 39.4 83.6 83.8 96.6l102.5 30c12.5 3.7 21.2 15.3 21.2 28.3 0 16.3-13.2 29.5-29.5 29.5h-66.3C100 368 88 364.3 78 357.5c-6.1-4.1-14.3-3.1-19.5 2l-34.8 34c-7.1 6.9-6.1 18.4 1.8 24.5 24.5 19.2 55.1 29.9 86.5 30v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-48.2c46.6-.9 90.3-28.6 105.7-72.7 21.5-61.6-14.6-124.8-72.5-141.7z"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="grow ml-6">
+                        <p className="font-bold mb-1">Delivery Order</p>
+                        <p className="text-gray-500">+234-999-444-333</p>
+                        <p className="text-gray-500">+234-999-555-333</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
       <Footer />
     </div>
   );
