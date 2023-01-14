72942a4a95f94eb4934230340a60d96937a60e56
Ali Abbani <aliabbani08@gmail.com>
2023-01-14 17:03:09 +0100

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import GridRestaurants from "../components/GridRestaurants";
 import NavBar from "../components/NavBar";
 import Slider from "../components/Slider";
-import Logo from "../assets/images/logo.png";
+import Logo from "../assets/images/logo-colored.png";
 
 const Home = () => {
   return (
