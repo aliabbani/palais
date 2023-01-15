7511086c086901fc2af03010291f5bcd4a7b1453
Ali Abbani <aliabbani08@gmail.com>
2023-01-15 10:28:19 +0100

    up

--- src/pages/Lepalais.js
+++ src/pages/Lepalais.js
@@ -59,102 +59,35 @@ const Lepalais = () => {
           </div>
         </div>
       </div>
-      {/* <div className="appz">
-        <div className="app-header">
-          <div>le palais page</div>
-          <div>
-            <Carousel cols={4} rows={1} gap={0} loop>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://images.unsplash.com/photo-1574169208507-84376144848b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=579&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://plus.unsplash.com/premium_photo-1671209881232-40bcdd0ba886?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://images.unsplash.com/photo-1510906594845-bc082582c8cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=844&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://images.unsplash.com/photo-1552083974-186346191183?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://images.unsplash.com/photo-1488554378835-f7acf46e6c98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://images.unsplash.com/photo-1519751138087-5bf79df62d5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://plus.unsplash.com/premium_photo-1669160075657-559f4db75914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-image"
-                  src="https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                  alt=""
-                />
-              </Carousel.Item>
-            </Carousel>
-          </div>
+      <div className="mx-10">
+        <div>menu</div>
+        <div className="">
+          <Carousel
+            responsive={responsive}
+            swipeable={false}
+            draggable={false}
+            ssr={true}
+            infinite={true}
+            // keyBoardControl={true}
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            // deviceType={this.props.deviceType}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+          >
+            <div className="border border-black">Item 1</div>
+            <div className="border border-yellow-500">Item 2</div>
+            <div className="border border-red-200">Item 3</div>
+            <div className="border border-blue-300">Item 4</div>
+            <div className="border border-yellow-500">Item 5</div>
+            <div className="border border-blue-300">Item 6</div>
+            <div className="border border-red-200">Item 7</div>
+            <div className="border border-green-600">Item 8</div>
+            <div className="border border-black">Item 9</div>
+            <div className="border border-yellow-500">Item 10</div>
+          </Carousel>
         </div>
-      </div> */}
-      <Carousel
-        responsive={responsive}
-        swipeable={false}
-        draggable={false}
-        ssr={true}
-        infinite={true}
-        // keyBoardControl={true}
-        containerClass="carousel-container"
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        // deviceType={this.props.deviceType}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        <div className="border border-black">Item 1</div>
-        <div className="border border-yellow-500">Item 2</div>
-        <div className="border border-red-200">Item 3</div>
-        <div className="border border-blue-300">Item 4</div>
-        <div>Item 5</div>
-        <div>Item 6</div>
-        <div>Item 7</div>
-        <div>Item 8</div>
-        <div>Item 9</div>
-        <div>Item 10</div>
-      </Carousel>
+      </div>
       ;{/* Maps & contact */}
       <div className="container my-24 px-6 mx-auto">
         <section className="mb-32 text-gray-800">
