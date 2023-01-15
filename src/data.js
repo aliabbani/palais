ef6fb2ba8a4532c45fc1bfc9a4e0cce45fb68742
Ali Abbani <aliabbani08@gmail.com>
2023-01-15 20:26:50 +0100

    up

--- src/data.js
+++ src/data.js
@@ -64,3 +64,136 @@ export const palaisSnackSalades = [
     image: "https://i.ibb.co/f8VFxjP/batonet-mozerella.jpg",
   },
 ];
+
+export const palaisSandwiches = [
+  {
+    name: "Fajitas",
+    ingredient:
+      "Poitrine de poulet, poivron vert, oignon, champignons de Paris, maïs, soya sauce, sauce guacamole, sauce piquante, mozzarella",
+    price: "7.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Philadelphia",
+    ingredient:
+      "viande, poivron vert, oignon, champignon, maïs, soya sauce, mozzarella, sauce bbq, mayonaise",
+    price: "7.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Francisco",
+    ingredient:
+      "Poulet grillé, fromage mozzarella, maïs, cornichon, laitue, sauce mayonnaise, soya sauce",
+    price: "7.00",
+    desc: "",
+    image: "https://i.ibb.co/f8VFxjP/batonet-mozerella.jpg",
+  },
+  {
+    name: "Sandwich Frites",
+    ingredient: "Frites, choux mayonnaise, ketchup, cornichons",
+    price: "3.50",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Sandwich Quésadillas",
+    ingredient:
+      "Poitrine de poulet, oignons, poivrons, fromage mozzarella, fromage cheddar, parmesan, sauce guacamole, sauce piquante,  champignon, mais, et olives, pain tortillas",
+    price: "8.50",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Sandwich Twister Au Poulet",
+    ingredient:
+      "Poitrine de poulet mariné, iceberg, mayonnaise, pain tortillas, fromage cheddar, tomate",
+    price: "6.50",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Sandwich Falafel",
+    ingredient: "Tomate, cornichons, persil, sauce sésame, menthe et radis",
+    price: "6.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Sandwich Chicken Sub",
+    ingredient:
+      "Poitrine de poulet, aioli sauce, cornichons, mais, iceberg, soya sauce, mozzarella, mayonaise",
+    price: "7.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Steak Sandwich",
+    ingredient:
+      "Viande steak, champignon, mayonaise, cornichons, iceberg, mais, bbq sauce",
+    price: "8.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Sandwich Chicken Pesto",
+    ingredient:
+      "Poitrine de poulet, mix fromage, pesto sauce, cornichons, tomate, mais, iceberg",
+    price: "8.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Sandwich soujouk",
+    ingredient:
+      "saucisses libanaise poelees, tomate, cornichon, ail, mayonaise, laitue, jus de citron",
+    price: "6.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Sandwich Makanek",
+    ingredient:
+      "saucisses libanaise poelees, tomate, cornichon, ail, mayonaise, laitue, jus de citron",
+    price: "6.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "Plat falafel",
+    ingredient:
+      "mélange legumineuse epice, sauce sesame, legume frais, cornichon",
+    price: "12.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "SANDWICH DINDE",
+    ingredient: "",
+    price: "3.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "SANDWICH FROMAGE",
+    ingredient: "",
+    price: "3.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "SANDWICH SALAMI",
+    ingredient: "",
+    price: "3.00",
+    desc: "",
+    image: "",
+  },
+  {
+    name: "SANDWICH LABNEH",
+    ingredient: "",
+    price: "3.00",
+    desc: "",
+    image: "https://i.ibb.co/xmPVgrN/chicken-fajita-sandwich.jpg",
+  },
+];
