62e7a242a1e0cd5e5d2d776a4926e030e344f88e
Ali Abbani <aliabbani08@gmail.com>
2023-01-21 16:42:34 +0100

    up

--- src/components/GridRestaurants.js
+++ src/components/GridRestaurants.js
@@ -27,7 +27,7 @@ function GridRestaurants() {
                 type="button"
                 className="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition-property duration-150 ease-in-out"
               >
-                See Mores
+                See More
               </button>
             </Link>
           </div>
