e26892ccdd0bdb891529dbeeedd35f8b9f2a726b
Ali Abbani <aliabbani08@gmail.com>
2023-01-21 01:28:18 +0100

    add form for cart de fidelite

--- src/pages/Loyality.js
+++ src/pages/Loyality.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import "../index.css";
 
 const Loyality = () => {
+  const [state, setState] = useState(false);
+
   return (
     <div>
       <NavBar />
@@ -27,10 +29,70 @@ const Loyality = () => {
         </div>
         <button
           type="submit"
-          className="bg-[#ffbc0d] rounded-md border-none py-3 px-4 text-center no-underline text-base text-[#292929] outline-none w-32"
+          className={`bg-[#ffbc0d] rounded-md border-none py-3 px-4 text-center no-underline text-base text-[#292929] outline-none w-32 ${
+            state ? "hidden" : "block"
+          }`}
+          onClick={() => setState(true)}
         >
           Apply Now
         </button>
+        {state ? (
+          <form action="https://formspree.io/f/mknanvrb" method="POST">
+            <div className="text-xl md:text-2xl font-black capitalize pt-6 pb-6 md:pt-20 pb-10">
+              your info
+            </div>
+            <div className="pb-6">
+              <input
+                className="h-10 border-b-[1px] border-gray-300 w-full text-normal font-semibold py-2 px-3 text-gray-700 placeholder-gray-400 !outline-none !ring-0"
+                type="text"
+                name="first name"
+                placeholder="*First Name"
+                required
+              />
+            </div>
+            <div className="pb-6">
+              <input
+                className="h-10 border-b-[1px] border-gray-300 w-full text-normal font-semibold py-2 px-3 text-gray-700 placeholder-gray-400 !outline-none !ring-0"
+                type="text"
+                name="last name"
+                placeholder="*Last Name"
+                required
+              />
+            </div>
+            <div className="pb-6">
+              <input
+                className="h-10 border-b-[1px] border-gray-300 w-full text-normal font-semibold py-2 px-3 text-gray-700 placeholder-gray-400 !outline-none !ring-0"
+                type="email"
+                name="email"
+                placeholder="*Personal Email"
+                required
+              />
+            </div>
+            <div className="pb-6">
+              <input
+                className="h-10 border-b-[1px] border-gray-300 w-full text-normal font-semibold py-2 px-3 text-gray-700 placeholder-gray-400 !outline-none !ring-0"
+                placeholder="*Phone Number"
+                bordered="false"
+                type="tel"
+                name="phone number"
+                pattern="[0-9]{9}"
+                required
+              />
+            </div>
+            <div>
+              By selecting Submit, any information you provide will be
+              protected. We may contact you regarding your submission.
+            </div>
+            <div className="py-20">
+              <button
+                type="submit"
+                className="bg-[#ffbc0d] rounded-md border-none py-3 px-8 text-center no-underline text-base text-[#292929] outline-none w-auto min-w-117"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        ) : null}
       </div>
       <Footer />
     </div>
