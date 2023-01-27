5346a9b5c1724c0e68861dc95ddb3f94e1b81647
Ali Abbani <aliabbani08@gmail.com>
2023-01-27 17:43:14 +0100

    up

--- src/pages/Loyality.js
+++ src/pages/Loyality.js
@@ -20,35 +20,42 @@ const Loyality = () => {
         </div>
         <div className="border-4 border-white p-6 leading-relaxed bg-[#d3d3d3] mb-10">
           Nous savons que vous êtes un client fidèle et nous apprécions votre
-          entreprise. C'est pourquoi nous offrons une remise spéciale sur le
-          menu grâce à notre programme de fidélité. Vous souhaitez bénéficier
-          d'une réduction sur votre prochaine expérience culinaire ? Nous sommes
-          là pour vous aider. Notre carte de fidélité offre une réduction sur
-          nos 4 restaurants. Ainsi, vous pouvez profiter de la nourriture que
-          vous aimez à un prix inférieur. Cliquez simplement sur Appliquer. Une
-          fois sur place, vous pourrez vous inscrire pour votre carte de
-          fidélité et commencer à profiter des avantages immédiatement !
+          entreprise. <br></br>
+          <br></br>
+          C'est pourquoi nous offrons une remise spéciale sur le menu grâce à
+          notre programme de fidélité.<br></br>
+          <br></br>
+          Vous souhaitez bénéficier d'une réduction sur votre prochaine
+          expérience culinaire ? <br></br>
+          <br></br>Nous sommes là pour vous aider.<br></br>
+          <br></br>Notre carte de fidélité offre une réduction sur nos 4
+          restaurants. Ainsi, vous pouvez profiter de la nourriture que vous
+          aimez à un prix inférieur.<br></br>
+          <br></br>
+          Cliquez simplement sur Appliquer maintenant. Une fois sur place, vous
+          pourrez vous inscrire pour votre carte de fidélité et commencer à
+          profiter des avantages immédiatement !
         </div>
         <button
           type="submit"
-          className={`bg-[#ffbc0d] rounded-md border-none py-3 px-4 text-center no-underline text-base text-[#292929] outline-none w-32 ${
+          className={`bg-[#ffbc0d] rounded-md border-none py-3 px-4 text-center no-underline text-base text-[#292929] outline-none w-52 ${
             state ? "hidden" : "block"
           }`}
           onClick={() => setState(true)}
         >
-          Apply Now
+          Appliquer maintenant
         </button>
         {state ? (
           <form action="https://formspree.io/f/mknanvrb" method="POST">
             <div className="text-xl md:text-2xl font-black capitalize pt-6 pb-6 md:pt-20 pb-10">
-              your info
+              vos informations
             </div>
             <div className="pb-6">
               <input
                 className="h-10 border-b-[1px] border-gray-300 w-full text-normal font-semibold py-2 px-3 text-gray-700 placeholder-gray-400 !outline-none !ring-0"
                 type="text"
-                name="first name"
-                placeholder="*First Name"
+                name="Prénom"
+                placeholder="*Prénom"
                 required
               />
             </div>
@@ -56,8 +63,8 @@ const Loyality = () => {
               <input
                 className="h-10 border-b-[1px] border-gray-300 w-full text-normal font-semibold py-2 px-3 text-gray-700 placeholder-gray-400 !outline-none !ring-0"
                 type="text"
-                name="last name"
-                placeholder="*Last Name"
+                name="Nom de famille"
+                placeholder="*Nom de famille"
                 required
               />
             </div>
@@ -66,31 +73,32 @@ const Loyality = () => {
                 className="h-10 border-b-[1px] border-gray-300 w-full text-normal font-semibold py-2 px-3 text-gray-700 placeholder-gray-400 !outline-none !ring-0"
                 type="email"
                 name="email"
-                placeholder="*Personal Email"
+                placeholder="*Email personnel"
                 required
               />
             </div>
             <div className="pb-6">
               <input
                 className="h-10 border-b-[1px] border-gray-300 w-full text-normal font-semibold py-2 px-3 text-gray-700 placeholder-gray-400 !outline-none !ring-0"
-                placeholder="*Phone Number"
+                placeholder="*Numéro de téléphone"
                 bordered="false"
                 type="tel"
-                name="phone number"
+                name="Numéro de téléphone"
                 pattern="[0-9]{9}"
                 required
               />
             </div>
             <div>
-              By selecting Submit, any information you provide will be
-              protected. We may contact you regarding your submission.
+              En sélectionnant Soumettre, toutes les informations que vous
+              fournissez seront protégées. Nous pouvons vous contacter au sujet
+              de votre soumission.
             </div>
             <div className="py-20">
               <button
                 type="submit"
                 className="bg-[#ffbc0d] rounded-md border-none py-3 px-8 text-center no-underline text-base text-[#292929] outline-none w-auto min-w-117"
               >
-                Submit
+                Soumettre
               </button>
             </div>
           </form>
