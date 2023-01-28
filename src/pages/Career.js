ee5e2724bacdd88a9de0148fa3d76640af848ec4
Ali Abbani <aliabbani08@gmail.com>
2023-01-28 18:10:21 +0100

    up

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 const Career = () => {
+  const [show, setShow] = React.useState(false);
   return (
     <div className="flex flex-col content-between">
       <NavBar />
@@ -39,22 +40,86 @@ const Career = () => {
           </div>
         </section>
       </div>
-      <div>career page</div>
+
       <div class="flex justify-center">
-        <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+        <div class="block p-6 rounded-lg shadow-lg bg-white ">
           <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
-            Card title
+            Cassier
           </h5>
           <p class="text-gray-700 text-base mb-4">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            We are looking for a cashier who is friendly, enthusiastic, and
+            outgoing. You'll be working with the public at a restaurant, so we
+            need someone who is comfortable interacting with people in a
+            fast-paced environment.
           </p>
+
+          <div>
+            <div>The Cashier will be responsible for:</div>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Making sure all cash transactions are completed correctly and
+              promptly.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Accepting payment from customers using a credit card or debit
+              card, or making change if necessary.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Redeem stamps, offers and coupons
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Maintaining an orderly work area by keeping it clean and
+              organized at all times.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - You should be able to multi-task by taking orders from customers
+              and ringing them up at the register.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Providing excellent customer service by greeting customers
+              politely, offering assistance when needed, and thanking them when
+              they leave the restaurant satisfied with their visit (or not).
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - You should have knowledge working on ordering system.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - You should be able to memorize the food menu, including the
+              ingredient in one months period.
+            </p>
+          </div>
+          <div>
+            <div>The Cashier qualification:</div>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Basic PC knowledge.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - 2 experience working as a cashier.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Familiarity with electronic equipment, like cash register and
+              POS
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Good math skills.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Strong communication and time management skills.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - Customer satisfaction-oriented.
+            </p>
+            <p className="text-gray-700 text-base mb-1 pl-4">
+              - bachelor degree or equivalent in business management,
+              accounting, or marketing.
+            </p>
+          </div>
           <button
             type="button"
-            class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
-            Button
+            Apply Now
           </button>
+          {show && <div></div>}
         </div>
       </div>
       <Footer />
---
+++ src/pages/careercashier.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const careercashier = () => {
+  return (
+    <div>
+       <div>
+              <div>The Cashier will be responsible for:</div>
+              <p className="text-gray-700 text-base mb-1 pl-4">
+                - Making sure all cash transactions are completed correctly and
+                promptly.
+              </p>
+              <p className="text-gray-700 text-base mb-1 pl-4">
+                - Accepting payment from customers using a credit card or debit
+                card, or making change if necessary.
+              </p>
+              <p className="text-gray-700 text-base mb-1 pl-4">
+                - Maintaining an orderly work area by keeping it clean and
+                organized at all times.
+              </p>
+              <p className="text-gray-700 text-base mb-1 pl-4">
+                - You should be able to multi-task by taking orders from
+                customers and ringing them up at the register.
+              </p>
+              <p className="text-gray-700 text-base mb-1 pl-4">
+                - Providing excellent customer service by greeting customers
+                politely, offering assistance when needed, and thanking them
+                when they leave the restaurant satisfied with their visit (or
+                not).
+              </p>
+              <p className="text-gray-700 text-base mb-1 pl-4">
+                - You should have knowledge working on ordering system.
+              </p>
+              <p className="text-gray-700 text-base mb-1 pl-4">
+                - You should be able to memorize the food menu, including the
+                ingredient in one months period.
+              </p>
+            </div>
+    </div>
+  )
+}
+
+export default careercashier
